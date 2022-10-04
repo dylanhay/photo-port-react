88e26a8d389e95452c13409df5cdc21e34258915
dchay <dylanhay@rogers.com>
2022-10-04 16:27:48 -0700

    error handling improved_rev3

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,20 +12,13 @@ function ContactForm() {
   const [errorMessage, setErrorMessage] = useState("");
   const { name, email, message } = formState;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log("Submit Form", formState);
-    }
-  };
-
-  const handleChange = (e) => {
+  function handleChange(e) {
     if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
         setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage("");
+      } else if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
       }
     } else {
       if (!e.target.value.length) {
@@ -38,7 +31,14 @@ function ContactForm() {
       setFormState({ ...formState, [e.target.name]: e.target.value });
       console.log("Handle Form", formState);
     }
-  };
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log("Submit Form", formState);
+    }
+  }
 
   return (
     <section>
